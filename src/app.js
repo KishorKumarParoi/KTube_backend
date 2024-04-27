7e55a8b192025b00fdb6f28bf345b7f6cf4c6d3d
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-04-27 21:27:10 +0600

    Utils wrapper handler

--- src/app.js
+++ src/app.js
@@ -1,25 +1,20 @@
-import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
-
-dotenv.config({
-    path: './src/.env'
-});
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+)
 
-app.get('/about', (req, res) => {
-    res.send('About page');
-});
+app.use(express.json({
+    limit: '16kb'
+}));
 
-console.log(process.env.PORT);
-console.log(process.env.MONGO_URI);
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
-
-
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
