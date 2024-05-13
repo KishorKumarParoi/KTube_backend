3ed4e9740ba92ee49bd656e0877abeccf62ce730
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-05-13 22:58:31 +0600

    user and video model created

---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true,
+            minlength: 3,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+export const User = mongoose.model("User", userSchema);
