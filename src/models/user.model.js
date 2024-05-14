cf69da90f8f434d3beb67ecb3cbe37398a4f0390
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-05-14 19:36:13 +0600

    generate refresh and access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
@@ -50,5 +52,43 @@ const userSchema = new Schema(
     }
 );
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, process.env.SALT_ROUNDS);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+        }
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
